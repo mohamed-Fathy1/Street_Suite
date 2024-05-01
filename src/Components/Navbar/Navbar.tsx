60867443c5c5526850c0272e544ffdcf0add7050
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-05-01 04:26:46 +0300

    Get the nav animation just right ;)

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -7,29 +7,41 @@ import AutomationIconMin from "../../assets/svg/mobile/AutomationIconMin";
 import NavbarItems from "../NavbarItems/NavbarItems";
 import NotificationBellIconMin from "../../assets/svg/mobile/NotificationBellIconMin";
 import UserProfileIcon from "../../assets/svg/mobile/UserProfileIcon";
+import styles from "./Navbar.module.css";
+import classNames from "classnames";
 
 const navBarItems = [
   {
+    id: 1,
+    title: "Alerts",
     icon: <AlertIconMin />,
     link: "#",
     active: true,
   },
   {
+    id: 2,
+    title: "Training",
     icon: <TrainingIconMin />,
     link: "#",
     active: true,
   },
   {
+    id: 3,
+    title: "Automation",
     icon: <AutomationIconMin />,
     link: "#",
     active: false,
   },
   {
+    id: 4,
+    title: "Portifolio",
     icon: <PortifolioIconMin />,
     link: "#",
     active: false,
   },
   {
+    id: 5,
+    title: "Trading",
     icon: <TradingIconMin />,
     link: "#",
     active: false,
@@ -39,15 +51,25 @@ const navBarItems = [
 function Navbar() {
   return (
     <nav className="w-12 h-dvh">
-      <div className="flex flex-col justify-between h-full w-max w-min-12 items-center bg-neutral-900 px-1">
-        <section className="flex flex-col">
-          <div className="flex items-center justify-center w-10 mt-6">
-            <img src={logo} alt="logo" className="w-full" />
+      <div
+        className={classNames(
+          "flex flex-col justify-between h-full w-max w-min-12 bg-neutral-900 px-1 group",
+          styles.navbar
+        )}
+      >
+        <section className="flex flex-col relative">
+          <div className="flex items-center justify-center mt-6">
+            <img
+              src={logo}
+              alt="logo"
+              className={classNames("", styles.nav_item_img)}
+            />
           </div>
           <ul className="flex flex-col items-center justify-center mt-11 gap-6">
-            {navBarItems.map((item, index) => (
+            {navBarItems.map((item) => (
               <NavbarItems
-                key={index}
+                key={item.id}
+                title={item.title}
                 icon={item.icon}
                 link={item.link}
                 active={item.active}
--- src/Components/NavbarItems/NavbarItems.tsx
+++ src/Components/NavbarItems/NavbarItems.tsx
@@ -1,16 +1,22 @@
 import classNames from "classnames";
-import styles from "./NavbarItems.module.css";
+import styles from "../Navbar/Navbar.module.css";
 
 interface NavbarItemsProps {
   icon: JSX.Element;
+  title: string;
   link: string;
   active: boolean;
 }
 
-function NavbarItems({ icon, link, active }: NavbarItemsProps) {
+function NavbarItems({ icon, link, active, title }: NavbarItemsProps) {
   return (
     <li className={classNames(styles.nav_item, active && styles.active)}>
-      <a href={link}>{icon}</a>
+      <a href={link} className="flex items-center">
+        {icon}
+        <span className={classNames("", styles.navbar_item_title)}>
+          {title}
+        </span>
+      </a>
     </li>
   );
 }
