58742d9425ac1ef3abd270e357704a7eaf5f21a4
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-05-02 10:46:21 +0300

    Add Filter layout

--- src/Pages/Alerts/Alerts.tsx
+++ src/Pages/Alerts/Alerts.tsx
@@ -1,4 +1,5 @@
 import ButtonPrimary from "../../Components/Buttons/ButtonPrimary";
+import Filter from "../../Components/Filter/Filter";
 import HeaderTitle from "../../Components/HeaderTitle";
 import SearchBar from "../../Components/SearchBar/SearchBar";
 import StockDataContainer from "../../Components/StockDataContainer/StockDataContainer";
@@ -7,24 +8,29 @@ import "./Alerts.css";
 
 function Alerts() {
   return (
-    <div className="@container px-3 pt-7 flex-1 lg:max-w-2xl max-h-dvh">
-      <header className="flex  items-center @[31em]:gap-5">
-        <HeaderTitle color="white" sideColor="#53ACFF">
-          Alerts
-        </HeaderTitle>
-        <SearchBar />
-        <div className="block @[31em]:hidden">
-          <ButtonPrimary size="sm" px={"23px"} py={"4px"}>
-            Filter
-          </ButtonPrimary>
-        </div>
-        <div className="hidden @[31em]:block">
-          <NotificationBellIcon />
-        </div>
-      </header>
-      <main className="mt-5 pr-2 overflow-auto main">
-        <StockDataContainer />
-      </main>
+    <div className="@container pl-3 pt-7 flex gap-5 flex-1 max-h-dvh">
+      <div className="lg:max-w-2xl flex-1">
+        <header className="flex  items-center @[31em]:gap-5">
+          <HeaderTitle color="white" sideColor="#53ACFF">
+            Alerts
+          </HeaderTitle>
+          <SearchBar />
+          <div className="block @[31em]:hidden">
+            <ButtonPrimary size="sm" px={"23px"} py={"4px"}>
+              Filter
+            </ButtonPrimary>
+          </div>
+          <div className="hidden @[31em]:block">
+            <NotificationBellIcon />
+          </div>
+        </header>
+        <main className="mt-5 pr-2 overflow-auto main">
+          <StockDataContainer />
+        </main>
+      </div>
+      <div className="h-full ml-auto">
+        <Filter />
+      </div>
     </div>
   );
 }
